f3d4ab4efc4c7eb9d504717c10f7c15dc4c53e38
chulleyboy <ryanmmassenburg@gmail.com>
2023-04-06 13:22:42 -0700

    fixed routes?

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // get notes from database
